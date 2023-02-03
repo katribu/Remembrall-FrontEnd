a14ba4e3f05522ce4577cdb254b9ee60a3bb941a
Irgen Wiig Sørensen <67002417+IrgenWiigSorensen@users.noreply.github.com>
2023-02-03 13:53:42 +0100

    Connected Frontend to the api

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,14 +1,46 @@
+import { Component } from "react";
 import { getUsersFromServer } from "../functions/fetch";
 
 
 
-export async function Users() {
-// need to use Hooks if we are to use functional components with async JS!!!
-    const users = await getUsersFromServer(); 
+export class Users extends Component {
 
-    const user =  await users.map(user => {
-        return <h1>{user.name}</h1>
-    })
-    console.log(user)
-    return user;
+// need to use Hooks if we are to use functional components with async JS!!!
+
+constructor(props) {
+    super(props); 
+
+    this.state = {
+        users: [],
+    }
+
+}
+
+async componentDidMount() {
+    const usersArray = await getUsersFromServer(); 
+
+    this.setState({
+        users: usersArray
+    }) 
+
+}
+
+render() {
+        const {users} = this.state
+      
+        const user = users.map((user) => {
+        return (
+        <div>
+            <h1>{user.name}</h1>
+         </div>
+         
+        )
+        })
+
+
+        return(
+            <div>{user}</div>
+        )
+    }
+
 }

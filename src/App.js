917cc64099c90716bfd5822dd92b35024a98f457
ShahinHemat <shahinhemat@gmail.com>
2023-02-03 14:10:34 +0100

    OCD cleanup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
-// import { BrowserRouter, Switch, Route } from 'react-router-dom'
+// Import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { Users } from './components/Users';
 import React from "react"
 
-// Hello 
+
 function App() {
   return (
     <div className="App">
+
       <div>
-      <h2>Hello, it works</h2>
+        <h2>Hello, it works</h2>
       </div>
 
       <div>
-      <Users/>
+        <Users />
       </div>
+      
     </div>
   );
 }
